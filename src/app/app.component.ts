53cd303c25b184daa648c0ada0ffa01f4d6d1270
Salomeleshka <salomeleshka@gmail.com>
2022-04-21 21:06:29 +0200

    initial project files

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  // styles: [
+  //   `
+  //     h3 {
+  //       color: lime;
+  //     }
+  //   `,
+  // ],
+})
+export class AppComponent {
+  username = '';
+  showSecret = false;
+  log = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
+  }
+}
